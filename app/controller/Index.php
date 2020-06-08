e7f4967f30c0ca199f1b804e96496e890aebe144
chenli <chenli@haituncun.com>
2020-06-08 23:12:47 +0800

    支付示例

--- app/controller/Index.php
+++ app/controller/Index.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace app\controller;
 
 use app\BaseController;
@@ -7,11 +8,70 @@ class Index extends BaseController
 {
     public function index()
     {
-        return '<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px;} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:) 2020新春快乐</h1><p> ThinkPHP V' . \think\facade\App::version() . '<br/><span style="font-size:30px;">14载初心不改 - 你值得信赖的PHP框架</span></p><span style="font-size:25px;">[ V6.0 版本由 <a href="https://www.yisu.com/" target="yisu">亿速云</a> 独家赞助发布 ]</span></div><script type="text/javascript" src="https://tajs.qq.com/stats?sId=64890268" charset="UTF-8"></script><script type="text/javascript" src="https://e.topthink.com/Public/static/client.js"></script><think id="ee9b1aa918103c4fc"></think>';
+        $orgCode = ['alipay', 'epayments', 'allpay'];
+        $methodCode = [
+            ['name' => 'alipay', 'children' =>
+                [
+                    ['name' => 'alipay_payment', 'val' => '支付宝']
+                ]
+            ],
+            ['name' => 'epayments', 'children' =>
+                [
+                    ['name' => 'wechat', 'val' => '微信']
+                ]
+            ],
+            ['name' => 'allpay', 'children' =>
+                [
+                    ['name' => 'alipay_payment', 'val' => '支付宝'],
+                    ['name' => 'vtpayment_payment', 'val' => '银联']
+                ],
+            ],
+        ];
+        $orderId = date('YmdHis') + mt_rand(100, 999);
+
+        return view('index', ['orderId' => $orderId, 'orgCode' => $orgCode, 'methodCode' => json_encode($methodCode, JSON_UNESCAPED_UNICODE)]);
     }
 
-    public function hello($name = 'ThinkPHP6')
+
+    public function pay()
     {
-        return 'hello,' . $name;
+        $param = [];
+        $param['org_code'] = input('post.org_code');
+        if (!$param['org_code']) {
+            return '请选择支付机构';
+        }
+
+        $param['method_code'] = input('post.method_code');
+        if (!$param['method_code']) {
+            return '请选择支付方式';
+        }
+
+        $param['order_id'] = input('post.order_id');
+        if (!$param['order_id']) {
+            return '请输入订单号';
+        }
+
+        $param['total_fee'] = input('post.total_fee');
+        if (!$param['total_fee']) {
+            return '支付金额';
+        }
+
+        $param['currency'] = 'AUD';
+
+        $param['user_agent_type'] = '1';
+        if (isMobile()) {
+            $param['user_agent_type'] = '2';
+        }
+
+        $curl = new \Curl\Curl();
+        $host = env('payment_demo.host');
+        $curl->post($host . '/payment/order/submit', $param);
+        $result = json_decode($curl->response, true);
+        if ($result['code'] == 0) {
+            return $result['data'];
+        }
+
+        return $result['message'];
+
     }
 }
